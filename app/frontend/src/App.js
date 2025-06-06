9e8afaa5cb6cf1a81a9b82c954758aab2af1cbdc
Uzair <149545730+Uzi0312@users.noreply.github.com>
2025-06-06 07:23:42 +0530

    Add files via upload

---
+++ app/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ImageSearch from './components/ImageSearch';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1> StyleU: Visual Search</h1>
+      <ImageSearch />
+    </div>
+  );
+}
+
+export default App;
