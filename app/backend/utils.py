0012bf44d43b0fa00c3a38f0bd2b7d7e52ed5961
Uzair <149545730+Uzi0312@users.noreply.github.com>
2025-06-06 07:52:10 +0530

    Update utils.py

--- app/backend/utils.py
+++ app/backend/utils.py
@@ -8,8 +8,9 @@ from dotenv import load_dotenv
 import os
 
 
-load_dotenv('gem.env')
-genai.configure(api_key=os.getenv("api_key"))
+api_key = os.environ.get("GENAI_API_KEY")
+# Configure Gemini
+genai.configure(api_key=api_key)
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 # Loading embeddings at startup
